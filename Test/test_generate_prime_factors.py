98e7424f66215976d3eb7c7fb3c9d0c2990143d1
SheridanCollege <jasvinder.kalra@sheridancollege.ca>
2024-07-05 06:16:54 -0400

    A test to get an empty list with integer of 1

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,8 +1,24 @@
-#pylint: disable = missing - docstring
+
 """
 The test module for prime factors
 """
-# Import pytest
-#from prime import generate_prime_factors
+
+import pytest
+import prime
+
+def generate_prime_factors():
+    """
+    A test to see a ValueError get raised if the user enters a non-integer data type
 
 
+    """
+    with pytest.raises(ValueError):
+        prime.generate_prime_factors(3.14)
+    with pytest.rases(ValueError):
+        prime.generate_prime_factors("abc")
+def test_integer_1_empty_list():
+    """
+    A test to get an empty list with interger of 1
+
+    """
+    assert prime.generate_prime_factors(1) == []
