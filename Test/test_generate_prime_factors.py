86af15036d1ebc4305d5f2fb81e7e7253bb624a2
SheridanCollege <jasvinder.kalra@sheridancollege.ca>
2024-07-05 06:07:39 -0400

    prime_factors

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,8 @@
+#pylint: disable = missing - docstring
+"""
+The test module for prime factors
+"""
+# Import pytest
+#from prime import generate_prime_factors
+
 
--- prime.py
+++ prime.py
@@ -1,3 +1,12 @@
 """
 prime.py -- Write the application code here
 """
+def generate_prime_factors(n):
+    prime = []
+    divisor = 2
+    while divisor <=n:
+        if n% divisor == 0:
+            prime.append(divisor)
+        else:
+            divisor +=1
+    return  prime
